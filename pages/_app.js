da3a72d2b90781f5699d4fb12aa8e2e3186954d1
UrielArriaga <71513166+UrielArriaga@users.noreply.github.com>
2021-08-30 20:57:38 -0500

    initial web app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import NextApp from "next/app";
+import React from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+const theme = createTheme({
+  root: {
+    primary: "#26a69a",
+  },
+  palette: {
+    primary: {
+      light: "#0288d1",
+      main: "#F65B27",
+      dark: "#0288d1",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#ffff",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+    other: {
+      light: "#ffff",
+      main: "#ba000d",
+    },
+  },
+});
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+export default class App extends NextApp {
+  // remove it here
+  componentDidMount() {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles);
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
 }
-
-export default MyApp
